f11e98976407766998cc390e17c404ad6ec7ec27
Momir Stanišić <momir.stanisic.ns@gmail.com>
2024-12-18 12:36:20 +0100

    fix: sort listing images

--- eventPlanner/src/main/java/wedoevents/eventplanner/listingManagement/services/ListingService.java
+++ eventPlanner/src/main/java/wedoevents/eventplanner/listingManagement/services/ListingService.java
@@ -35,6 +35,7 @@ public class ListingService {
             else if (sortBy != null && sortBy.equalsIgnoreCase("price"))
                 listings.sort(Comparator.comparing(ListingDTO::getPrice).reversed());
         }
+        listings.forEach(listing -> listing.getImages().sort(String::compareTo));
         int totalPages = data.isEmpty() ? 1 : (int) Math.ceil((double) (Long) data.get(0)[data.get(0).length - 1] / size);
         return Map.of("totalPages", totalPages, "content", listings);
     }
