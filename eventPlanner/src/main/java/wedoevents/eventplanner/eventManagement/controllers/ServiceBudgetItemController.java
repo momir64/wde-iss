cc6a09e2a0935c0ca32613a53818bf2258d23632
Luka <79410671+lukascobbler@users.noreply.github.com>
2024-12-19 22:53:02 +0100

    feature: added skeleton for service buying / reservation

--- eventPlanner/src/main/java/wedoevents/eventplanner/eventManagement/controllers/ServiceBudgetItemController.java
+++ eventPlanner/src/main/java/wedoevents/eventplanner/eventManagement/controllers/ServiceBudgetItemController.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import wedoevents.eventplanner.eventManagement.dtos.BuyProductDTO;
+import wedoevents.eventplanner.eventManagement.dtos.BuyServiceDTO;
 import wedoevents.eventplanner.eventManagement.dtos.CreateServiceBudgetItemDTO;
+import wedoevents.eventplanner.eventManagement.dtos.ServiceBudgetItemDTO;
 import wedoevents.eventplanner.eventManagement.models.ServiceBudgetItem;
 import wedoevents.eventplanner.eventManagement.services.ServiceBudgetItemService;
+import wedoevents.eventplanner.shared.Exceptions.BuyProductException;
 import wedoevents.eventplanner.shared.Exceptions.EntityCannotBeDeletedException;
 
 import java.util.List;
@@ -26,9 +30,10 @@ public class ServiceBudgetItemController {
     }
 
     @PostMapping
-    public ResponseEntity<CreateServiceBudgetItemDTO> createOrUpdateServiceBudgetItem(@RequestBody CreateServiceBudgetItemDTO serviceBudgetItem) {
+    public ResponseEntity<?> createServiceBudgetItem(@RequestBody CreateServiceBudgetItemDTO serviceBudgetItem) {
+        // todo clean up this comment after checking
 //  if (adding new budget item - event doesn't have item with that category) {
-        return new ResponseEntity<>(serviceBudgetItem, HttpStatus.CREATED);
+//        return new ResponseEntity<>(serviceBudgetItem, HttpStatus.CREATED);
 //  } else if (error - some id isn't null but doesn't exist) {
 //      return new ResponseEntity<>(serviceBudgetItem, HttpStatus.NOT_FOUND);
 //  } else if (error - user doesn't have necessary permissions) {
@@ -42,6 +47,24 @@ public class ServiceBudgetItemController {
 //  } else if (buying service error - service id isn't null, event has item with that category and with service id) {
 //      return new ResponseEntity<>(serviceBudgetItem, HttpStatus.BAD_REQUEST);
 //  }
+        try {
+            return ResponseEntity.ok(serviceBudgetItemService.createServiceBudgetItem(serviceBudgetItem));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (BuyProductException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/buy")
+    public ResponseEntity<?> buyService(@RequestBody BuyServiceDTO buyServiceDTO) {
+        try {
+            return ResponseEntity.ok(serviceBudgetItemService.buyService(buyServiceDTO));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (BuyProductException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
     }
 
     @DeleteMapping ("/{eventId}/{serviceCategoryId}")
--- eventPlanner/src/main/java/wedoevents/eventplanner/eventManagement/dtos/CreateServiceBudgetItemDTO.java
+++ eventPlanner/src/main/java/wedoevents/eventplanner/eventManagement/dtos/CreateServiceBudgetItemDTO.java
@@ -1,5 +1,6 @@
 package wedoevents.eventplanner.eventManagement.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,9 @@ import java.util.UUID;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class CreateServiceBudgetItemDTO {
+    private UUID eventId;
     private UUID serviceCategoryId;
     private Double maxPrice;
 }
