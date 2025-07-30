cea9cfe525fb0e02cdf1a9abcb67f472945a1225
Momir Stanišić <momir.stanisic.ns@gmail.com>
2025-07-30 23:14:31 +0200

    hotfix: revert permission hotfix

--- eventPlanner/src/main/java/wedoevents/eventplanner/shared/config/auth/SecurityConfig.java
+++ eventPlanner/src/main/java/wedoevents/eventplanner/shared/config/auth/SecurityConfig.java
@@ -7,14 +7,10 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+ import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,44 +20,31 @@ import wedoevents.eventplanner.shared.services.auth.CustomUserDetailsService;
 @EnableWebSecurity(debug = true)
 @EnableMethodSecurity
 public class SecurityConfig {
-
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final AuthEntryPoint authEntryPoint;
 
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthEntryPoint authEntryPoint) {
-        this.authEntryPoint = authEntryPoint;
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/v3/api-docs.yaml", "/swagger-ui.html", "/swagger-ui/**",
-                                "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**", "/api-docs/**",
-                                "/api/v1/auth/**",
-                                "/api/v1/chats/**",
-                                "/api/v1/profiles/registration", "/api/v1/events/*/pdf",
-                                "/api/v1/profiles/images/**",
-                                "/api/v1/sellers/**",
-                                "/api/v1/events/top", "/api/v1/listings/top", "/api/v1/listings",
-                                "/api/v1/notifications**", "api/v1/products/**", "api/v1/services/**", "api/v1/events/**",
-                                "chat-socket/**"
-                        ).permitAll()
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/v1/admins").hasAnyRole("GUEST", "ADMIN", "ORGANIZER")
-                        .anyRequest().authenticated()
-                )
-                .exceptionHandling(ex -> ex
-                        .authenticationEntryPoint(authEntryPoint)
-                )
-                .sessionManagement(sess -> sess
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/v3/api-docs.yaml", "/swagger-ui.html", "/swagger-ui/**",
+                                     "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**", "/api-docs/**",
+                                     "/api/v1/auth/**",
+                                     "/api/v1/chats/**",
+                                     "/api/v1/profiles/registration", "/api/v1/events/*/pdf",
+                                     "/api/v1/profiles/images/**",
+                                     "/api/v1/sellers/**",
+                                     "/api/v1/events/top", "/api/v1/listings/top", "/api/v1/listings",
+                                     "/api/v1/notifications**", "/api/v1/products/**", "/api/v1/services/**", "/api/v1/events/**",
+                                     "/chat-socket/**").permitAll()
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/api/v1/admins").hasAnyRole("GUEST", "ADMIN", "ORGANIZER")
+                    .anyRequest().permitAll())
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -76,10 +59,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(
-            CustomUserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
-
+    public DaoAuthenticationProvider authenticationProvider(CustomUserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder);
